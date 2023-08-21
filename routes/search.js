2800a9764dd8ef7eb811a5d1bdf85569e39dc30b
higayoshiko <higayoshiko@yahoo.com>
2023-08-21 22:42:48 +0900

    add another page for customized search

--- routes/search.js
+++ routes/search.js
@@ -2,5 +2,24 @@ const express = require("express");
 const router = express.Router();
 const https = require('node:https');
 
+router.get("/search", function(req, res) {
+const path = "https://fakestoreapi.com/products";
+
+  https.get(path, (response) => {
+      let result = "";
+      response.on("data", function(data) {
+      result += data;
+          });
+
+      response.on("end", function() {
+        const parsed = JSON.parse(result);
+
+        res.render("home", {
+          products: parsed
+      });
+    });
+  });
+});
+
 
 module.exports = router;
