590ba4d3e61951db57ffae77f1ab68ef1d91e85e
higayoshiko <higayoshiko@yahoo.com>
2023-09-03 21:27:08 +0900

    implement add to cart

--- routes/home.js
+++ routes/home.js
@@ -1,6 +1,22 @@
 const express = require("express");
 const router = express.Router();
 const https = require('node:https');
+const cartItems = require("../models/CartItems");
+
+
+router.post("/", function(req, res) {
+
+// const cartItem = new CartItems({
+//   title: req.body.itemName,
+//   price: req.body.itemPrice,
+//   image: req.body.itemImage
+//   });
+
+// cartItem.save(err => {
+//   if(err) {console.log(err)}
+//   });
+console.log(req.body.itemName)
+});
 
 router.post("/", function(req, res) {
   let category = req.body.category;
--- routes/search.js
+++ routes/search.js
@@ -1,6 +1,19 @@
 const express = require("express");
 const router = express.Router();
 const https = require('node:https');
+const cartItems = require("../models/CartItems");
+
+
+router.post("/", function(req, res) {
+
+const cartItem = new CartItems({
+  title: req.body.itemName,
+  price: req.body.itemPrice,
+  image: req.body.itemImage
+  });
+
+  console.log(req)
+});
 
 router.get("/:category", function(req, res) {
   let category = req.params.category;
